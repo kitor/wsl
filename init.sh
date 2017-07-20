38a375e19a3b357f91df48df7de66601aa6a391c
Kajetan Krykwinski <kontakt@kitor.pl>
2017-07-20 11:20:31 +0200

    Added features for running rsyslog

--- init.sh
+++ init.sh
@@ -8,17 +8,22 @@ if [ -f "$pidfile" ] && kill -0 `cat $pidfile` 2>/dev/null; then
 fi
 echo $$ > $pidfile
 
-
-# run services
-service ssh start
-service sendmail start
-service cron start
-service dbus start
-
 # add permission to write for all to /run
 # /var/run is symlink to /run
 # without this eg. screen will not work as non-root user
 chmod go+w /run
+
+# and fix some permissions for rsyslog
+chmod g+w /dev/xconsole
+chmod 770 /var/log
+chgrp syslog /var/log
+
+# run services
+service rsyslog start
+service ssh start
+service sendmail start
+service cron start
+service dbus start
 
 # loop to keep tilda running
 while [ true ]; do
